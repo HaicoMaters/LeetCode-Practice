1e6c69175db9c14eb7678fa029d4aa9f61d19ff4
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-02 17:41:54 +0000

    Create file

---
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        s = s.Trim(); // Remove extra whitespace trailing and leading
+        string[] words = s.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries); // split by spaces
+        Array.Reverse(words); // reverse the words
+
+        return string.Join(" ", words); // join as a single string
+    }
+}
