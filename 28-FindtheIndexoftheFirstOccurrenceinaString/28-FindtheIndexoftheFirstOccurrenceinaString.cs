7a0f51d6189077543f27404daa1233c3762a3321
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-02 17:31:59 +0000

    Create file

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (needle.Length == 0) {
+            return 0;
+        }
+
+        if (needle.Length > haystack.Length) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.Length - needle.Length; i++) {
+            bool match = true;
+            for (int j = 0; j < needle.Length; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
