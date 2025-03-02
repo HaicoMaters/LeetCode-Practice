a716c5df5e57ac3db370336761d879250fe95e58
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-02 17:48:14 +0000

    Create file

---
+++ 709-ToLowerCase/709-ToLowerCase.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public string ToLowerCase(string s) {
+        var str = new StringBuilder();
+        for(int i = 0; i < s.Length; i++) 
+        {
+            int ascii = (int)s[i];
+            if(ascii >= 65 && ascii <= 90) 
+            { // Check if upper or lower Upper is 65 to 90 and lower is 97 to 122 a diff of 32
+                ascii += 32;
+                char a = (char)ascii;
+                str.Append(a);
+            } 
+            else 
+            {
+                char a = (char)ascii;
+                str.Append(a);
+            }
+        }
+        return str.ToString();
+    }
+}
