e38d83a793ec8f6e6be3eb3bb8530cb60ffa1c91
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-03 14:48:19 +0000

    Create file

---
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        var pq = new PriorityQueue<int, int>();
+
+        foreach (int num in nums)
+        {
+            pq.Enqueue(num, -num); // - prio for correct dequeue behaviour
+        }
+
+        for (int i = 1; i < k; i++)
+        {
+            pq.Dequeue();
+        }
+
+        return pq.Dequeue();
+    }
+}
