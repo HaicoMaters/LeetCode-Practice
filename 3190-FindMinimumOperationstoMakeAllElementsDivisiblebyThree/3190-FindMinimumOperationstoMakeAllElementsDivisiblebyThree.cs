cfb23ad03d7e9a3b85489325fff81e8638b99756
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-06 18:20:11 +0000

    Create file

---
+++ 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MinimumOperations(int[] nums) {
+        int operations = 0;
+        for (int i = 0; i < nums.Length; i++){
+            int remainder = nums[i] % 3;
+            
+            if (remainder == 0) continue;
+            else operations++; // will either be 1 or 2 which would make - 1 or + 1 takes only one action (do not need to worry about 0 in testcases)
+        }
+
+        return operations;
+    }
+}
