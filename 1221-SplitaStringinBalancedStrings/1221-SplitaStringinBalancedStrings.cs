e0bcb1b5191b8dd0302014d03c9587660b3a1ef4
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-10 16:31:39 +0000

    Create file

---
+++ 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int BalancedStringSplit(string s) { // Only inputs are L or R
+        int balance = 0;
+        int substrings = 0;
+        foreach (char c in s){
+            if (c == 'L'){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+
+            if (balance == 0) substrings++;
+        }
+
+        return substrings;
+    }
+}
