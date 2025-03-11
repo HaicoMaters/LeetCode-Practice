9a89e986da9edaf62dbe3346870a28108dda1ded
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-11 15:33:55 +0000

    Create file

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        HashSet<int> possible = new HashSet<int>();
+        HashSet<int> intersections = new HashSet<int>();
+
+        foreach (int num in nums1){
+            possible.Add(num);
+        }
+
+        foreach (int num in nums2){
+            if (possible.Contains(num)){
+                intersections.Add(num);
+            }
+        }
+
+        return intersections.ToArray();
+    }
+}
