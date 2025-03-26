a089e22a9280f47b54c730f52f42103c9a3272c4
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-03-26 15:51:15 +0000

    Create file

---
+++ 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.cs
@@ -0,0 +1,44 @@
+// Last updated: 26/03/2025, 15:51:15
+public class Solution {
+    public int NumberOfSubarrays(int[] nums, int k) {
+
+        int subarrayCount = 0;
+        int oddCount = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            if(nums[right] % 2 != 0)
+            {
+                oddCount++;
+            }
+
+            if(oddCount == k) // New Subbarray found
+            {
+                subarrayCount++;
+
+                int innerCount = left;
+
+                while(nums[innerCount] % 2 == 0) // Extend rightwards until find the next odd occurance
+                {
+                    innerCount++;
+                    subarrayCount++;
+                }
+            }
+
+            if(right + 1 < nums.Length && nums[right + 1] % 2 != 0 && oddCount == k) 
+            {
+                // shrink window moving left to right until odd count is equal to k after next odd
+                while(nums[left] % 2 == 0)
+                {
+                    left++;
+                }
+
+                left++;
+                oddCount--;
+            }
+        }
+
+        return subarrayCount;
+    }
+}
