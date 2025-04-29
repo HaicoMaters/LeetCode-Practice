93b612d25b635a8e1ba2fcfe7fdb924d08b0a6a3
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-04-29 13:42:50 +0100

    Create file

---
+++ 2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes/version/csharp/2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes_20250429_134249.cs
@@ -0,0 +1,35 @@
+// Last updated: 29/04/2025, 13:42:49
+public class Solution {
+    public long CountSubarrays(int[] nums, int k) {
+
+        int L = 0;
+        long subarrays = 0;
+        int maxElement = 0; // nums[i] >= 1
+        long maxOccur = 0; // num of occurances of max element in current window
+
+        foreach (int num in nums)
+        {
+            maxElement = Math.Max(maxElement, num);
+        }
+
+        for (int R = 0; R < nums.Length; R++)
+        {
+            if (nums[R] == maxElement)
+            {
+                maxOccur++;
+            }
+
+            while(maxOccur >= k) // shrink window from left
+            {
+                subarrays += nums.Length - R; // All subarrays ending at or after r
+                if (nums[L] == maxElement)
+                {
+                    maxOccur--;
+                }
+                L++;
+            }
+        }
+
+        return subarrays;
+    }
+}
