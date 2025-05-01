7650b702fe68dc51715ef80ae111f3a1f6d0c565
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-01 11:19:22 +0100

    Create file

---
+++ 144-BinaryTreePreorderTraversal/version/csharp/144-BinaryTreePreorderTraversal_20250501_111917.cs
@@ -0,0 +1,31 @@
+// Last updated: 01/05/2025, 11:19:17
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    List<int> order;
+    public IList<int> PostorderTraversal(TreeNode root) {
+        order = new List<int>();
+        DFS(root);
+        return order;
+    }
+
+    public void DFS(TreeNode current)
+    {
+        if (current == null) return;
+
+        DFS(current.left);
+        DFS(current.right);
+        order.Add(current.val);
+    }
+}
