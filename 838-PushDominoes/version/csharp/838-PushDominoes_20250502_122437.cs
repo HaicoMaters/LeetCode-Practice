7d8352527bb0f6df0c5209149ac525e7c2a6d29e
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-02 12:24:39 +0100

    Create file

---
+++ 838-PushDominoes/version/csharp/838-PushDominoes_20250502_122437.cs
@@ -0,0 +1,54 @@
+// Last updated: 02/05/2025, 12:24:37
+public class Solution {
+    public string PushDominoes(string dominoes) {
+        int n = dominoes.Length;
+        int[] forces = new int[n];
+
+        // Sweep from left to right
+        int force = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            if (dominoes[i] == 'R') 
+            {
+                force = n;
+            } else if (dominoes[i] == 'L') 
+            {
+                force = 0;
+            } else if (force > 0) 
+            {
+                force--;
+            }
+            forces[i] += force;
+        }
+
+        // Sweep from right to left
+        force = 0;
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            if (dominoes[i] == 'L') 
+            {
+                force = n;
+            } 
+            else if (dominoes[i] == 'R') 
+            {
+                force = 0;
+            } 
+            else if (force > 0) 
+            {
+                force--;
+            }
+            forces[i] -= force;
+        }
+
+        // Build string from resulting forces
+        var result = new StringBuilder();
+        foreach (var f in forces) 
+        {
+            if (f > 0) result.Append('R');
+            else if (f < 0) result.Append('L');
+            else result.Append('.');
+        }
+
+        return result.ToString();
+    }
+}
