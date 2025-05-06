1baeb74b21dfd9c05260a3c119b8ee861fb5ffdd
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-06 12:15:07 +0100

    Create file

---
+++ 2679-SuminaMatrix/version/csharp/2679-SuminaMatrix_20250506_121507.cs
@@ -0,0 +1,28 @@
+// Last updated: 06/05/2025, 12:15:07
+public class Solution {
+    public int MatrixSum(int[][] nums) {
+        int n = nums.Length;
+        int m = nums[0].Length;
+
+        for(int i = 0; i < n; i++) 
+        {
+            Array.Sort(nums[i]);
+        }
+
+        int score = 0;
+        for (int j = 0; j < m; j++)
+        {
+            int max = nums[0][j];
+            for (int i = 1; i < n; i++)
+            {
+                if (nums[i][j] > max)
+                {
+                    max = nums[i][j];
+                }
+            }
+            score += max;
+        }
+
+        return score;
+    }
+}
