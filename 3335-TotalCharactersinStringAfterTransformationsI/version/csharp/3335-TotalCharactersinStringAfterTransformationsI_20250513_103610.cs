a53e68b6a65dd3cda129447160f3a8a29055ce6d
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-13 10:36:15 +0100

    Create file

---
+++ 3335-TotalCharactersinStringAfterTransformationsI/version/csharp/3335-TotalCharactersinStringAfterTransformationsI_20250513_103610.cs
@@ -0,0 +1,39 @@
+// Last updated: 13/05/2025, 10:36:10
+public class Solution {
+    public int LengthAfterTransformations(string s, int t) {
+        int mod = 1000000007;
+        long[] counts = new long[26];
+
+        foreach (char c in s)
+        {
+            counts[c - 'a']++;
+        }
+
+        while (t > 0)
+        {
+            long[] changed = new long[26];
+
+            for (int i = 0; i< 26; i++)
+            {
+                if (i == 25) // z -> ab
+                {
+                    changed[0] = (changed[0] + counts[i]) % mod;
+                    changed[1] = (changed[1] + counts[i]) % mod;
+                }
+                else // a-y -> b-z
+                {
+                    changed[i+1] = (changed[i+1] + counts[i]) % mod;
+                }
+            }
+            counts = changed;
+            t--;
+        }
+
+        long length = 0;
+        foreach (long c in counts)
+        {
+            length = (length + c) % mod;
+        }
+        return (int) length;
+    }
+}
