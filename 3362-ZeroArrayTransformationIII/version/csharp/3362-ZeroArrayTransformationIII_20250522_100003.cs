27d796677b1ad6e3ed043ff82ff84efb809dd3fb
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-22 10:00:05 +0100

    Create file

---
+++ 3362-ZeroArrayTransformationIII/version/csharp/3362-ZeroArrayTransformationIII_20250522_100003.cs
@@ -0,0 +1,35 @@
+// Last updated: 22/05/2025, 10:00:03
+public class Solution {
+    public int MaxRemoval(int[] nums, int[][] queries) {
+        int n = nums.Length;
+
+        Array.Sort(queries, (a, b) => a[0] - b[0]);
+
+        PriorityQueue<int,int> pq = new PriorityQueue<int, int>();
+
+        int[] changes = new int[n+1];
+        int operations = 0;
+
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            operations += changes[i];
+
+            while (j < queries.Length && queries[j][0] == i) // Enqueues all queries that start at current index i.
+            {
+                pq.Enqueue(queries[j][1], -queries[j][1]);
+                j++;
+            }
+            while (operations < nums[i] && pq.Count > 0 && pq.Peek() >= i) 
+            {
+                operations++;
+                changes[pq.Dequeue() + 1]--;
+            }
+            if (operations < nums[i])
+            {
+                return -1;
+            }
+        }
+        return pq.Count;
+    }
+}
