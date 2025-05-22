53020b59d0e794594952e36bc112abd2f773fbaf
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-22 13:14:35 +0100

    Create file

---
+++ 530-MinimumAbsoluteDifferenceinBST/version/csharp/530-MinimumAbsoluteDifferenceinBST_20250522_131435.cs
@@ -0,0 +1,32 @@
+// Last updated: 22/05/2025, 13:14:35
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    int minDiff = int.MaxValue;
+    TreeNode prev = null;
+    public int GetMinimumDifference(TreeNode root) {
+        DFS(root);
+        return minDiff;
+    }
+
+    public void DFS(TreeNode current)
+    {
+        if (current == null) return;
+
+        DFS(current.left);
+        if (prev != null) minDiff = (Math.Min(minDiff, current.val - prev.val));
+        prev = current;
+        DFS(current.right);
+    }
+}
