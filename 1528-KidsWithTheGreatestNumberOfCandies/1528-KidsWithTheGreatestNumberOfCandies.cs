5027ddfd686724270637a8b394ced3e2c1f01ed1
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:59 +0100

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.cs
@@ -0,0 +1,21 @@
+// Last updated: 28/05/2025, 13:14:58
+public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        int most = 0;
+
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (candies[i] > most){
+                most = candies[i];
+            }
+        }
+
+        List<bool> result = new List<bool>();
+
+        foreach (int kid in candies){
+            result.Add(kid + extraCandies >= most);
+        }
+
+        return result;
+    }
+}
