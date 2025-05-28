beb3ec1cf6787bcc34edc959fca2b2fafc066807
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:15:00 +0100

    Create 1464-ReduceArraySizeToTheHalf

---
+++ 1464-ReduceArraySizeToTheHalf/1464-ReduceArraySizeToTheHalf.cs
@@ -0,0 +1,35 @@
+// Last updated: 28/05/2025, 13:14:59
+public class Solution {
+    public int MinSetSize(int[] arr) {
+        int size = arr.Length;
+        int half = size / 2;
+
+        Dictionary<int, int> freq = new Dictionary<int, int>();
+        
+        foreach (int num in arr) 
+        {
+            if (!freq.ContainsKey(num)) 
+            {
+                freq[num] = 0;
+            }
+            freq[num]++;
+        }
+
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+        foreach (int val in freq.Values) 
+        {
+            pq.Enqueue(val, -val);
+        }
+
+        int minSetSize = 0;
+
+        while (size > half) 
+        {
+            size -= pq.Dequeue();
+            minSetSize++;
+        }
+
+        return minSetSize;
+    }
+}
