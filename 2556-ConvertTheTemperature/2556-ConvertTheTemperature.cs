3e059dcb216e43ee261f71d86c9e50b3ca4a222f
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:24 +0100

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.cs
@@ -0,0 +1,9 @@
+// Last updated: 28/05/2025, 13:14:23
+public class Solution {
+    public double[] ConvertTemperature(double celsius) {
+        double[] temp = new double[2];
+        temp[0] = celsius + 273.15;
+        temp[1] = celsius * 1.80 + 32.00;
+        return temp;
+    }
+}
