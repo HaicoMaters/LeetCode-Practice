15272bb271ab7d7d9a40b8275b1f7c5a3766cb67
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:16:47 +0100

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.cs
@@ -0,0 +1,25 @@
+// Last updated: 28/05/2025, 13:16:46
+public class Solution {
+    public char FindTheDifference(string s, string t) {
+        Dictionary<char, int> frequencies = new Dictionary<char, int>();
+
+        foreach (char c in s){
+            if(!frequencies.ContainsKey(c)){
+                frequencies[c] = 1;
+            }
+            else{
+                frequencies[c]++;
+            }
+        }
+        
+        foreach (char c in t)
+        {
+            if (!frequencies.ContainsKey(c) || frequencies[c] == 0){
+                return c;
+            }
+            frequencies[c]--;
+        }
+
+        return 'f';
+    }
+}
