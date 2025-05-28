fcfc8a1b258d5e4d25b29d9c5951397c956bcd88
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:32 +0100

    Create 2347-CountNodesEqualToAverageOfSubtree

---
+++ 2347-CountNodesEqualToAverageOfSubtree/2347-CountNodesEqualToAverageOfSubtree.cs
@@ -0,0 +1,40 @@
+// Last updated: 28/05/2025, 13:14:31
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    int count = 0;
+
+    public int AverageOfSubtree(TreeNode root) {
+        UpdateSubTreeCount(root);
+        return count;
+    }
+
+    public (int sum, int nodeCount) UpdateSubTreeCount(TreeNode current){
+        if (current == null){
+            return (0, 0);
+        }
+
+        var left = UpdateSubTreeCount(current.left);
+        var right = UpdateSubTreeCount(current.right);
+
+        int currentSum = left.sum + right.sum + current.val;
+        int currentCount = left.nodeCount + right.nodeCount + 1;
+
+        if ((currentSum / currentCount) == current.val){
+            count++;
+        }
+
+        return (currentSum, currentCount);
+    }
+}
