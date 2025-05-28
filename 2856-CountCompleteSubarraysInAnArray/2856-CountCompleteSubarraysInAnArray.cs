527957442e12af0e4ca67642b69579a21759810f
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:16 +0100

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.cs
@@ -0,0 +1,36 @@
+// Last updated: 28/05/2025, 13:14:15
+public class Solution {
+    public int CountCompleteSubarrays(int[] nums) {
+        int subarrays = 0;
+        int n = nums.Length;
+        Dictionary<int, int> numCounts = new Dictionary<int, int>();
+        HashSet<int> uniques = new HashSet<int>(nums);
+        int distinctNums = uniques.Count;
+        int l = 0;
+        int r = 0;
+
+        while (r < n)
+        {
+            if (!numCounts.ContainsKey(nums[r]))
+            {
+                numCounts[nums[r]] = 0;
+            }
+            numCounts[nums[r]]++;
+
+            while (numCounts.Count == distinctNums)
+            {
+                subarrays += n - r;
+                numCounts[nums[l]]--;
+                
+                if (numCounts[nums[l]] == 0)
+                {
+                    numCounts.Remove(nums[l]);
+                }
+                l++;
+            }
+            r++;
+        }
+
+        return subarrays;
+    }
+}
