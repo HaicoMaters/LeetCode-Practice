be06aafd4d5d7db2ca486fa47126e3206a8115c4
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:15:45 +0100

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.cs
@@ -0,0 +1,25 @@
+// Last updated: 28/05/2025, 13:15:44
+public class Solution {
+    public int LastStoneWeight(int[] stones) {
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+        foreach (int stone in stones)
+        {
+            pq.Enqueue(stone, -stone); // Start with heaviest
+        }
+
+        while (pq.Count > 1)
+        {
+            int size = pq.Dequeue() - pq.Dequeue(); // smash stones together to get size of combination
+
+            if (size != 0)
+            {
+                pq.Enqueue(size, -size);
+            }
+        }
+
+        if (pq.Count == 0) return 0;
+
+        return pq.Dequeue();
+    }
+}
