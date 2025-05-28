d3425753f20014d973594419757f4ad5a9b87b52
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:18:00 +0100

    Create 187-RepeatedDnaSequences

---
+++ 187-RepeatedDnaSequences/187-RepeatedDnaSequences.cs
@@ -0,0 +1,24 @@
+// Last updated: 28/05/2025, 13:17:59
+public class Solution {
+    public IList<string> FindRepeatedDnaSequences(string s) {
+
+        var sequences = new Dictionary<string, int>(); //Existing sequences
+        List<string> repeated = new List<string>();
+
+        for (int i = 0; i < s.Length - 9; i++)
+        {
+            string sub = s.Substring(i, 10);
+            if (!sequences.ContainsKey(sub))
+            {
+                sequences.Add(sub, 1);
+            }
+            else
+            {
+                if (sequences[sub] == 1) repeated.Add(sub);
+                sequences[sub]++;
+            }
+        }
+
+        return repeated;
+    }
+}
