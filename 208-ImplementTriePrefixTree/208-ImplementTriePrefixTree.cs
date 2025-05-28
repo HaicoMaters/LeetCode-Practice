4b1d2ea4ae69c67037eba8f9df293ecc69685cdb
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:17:52 +0100

    Create 208-ImplementTriePrefixTree

---
+++ 208-ImplementTriePrefixTree/208-ImplementTriePrefixTree.cs
@@ -0,0 +1,80 @@
+// Last updated: 28/05/2025, 13:17:51
+public class TrieNode{
+    
+    public Dictionary<char, TrieNode> children;
+    public bool isEndOfWord {get; set;}
+
+    public TrieNode(){
+        children = new Dictionary<char, TrieNode>();
+        isEndOfWord = false;
+    }
+}
+
+public class Trie {
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void Insert(string word) {
+        TrieNode currentNode = root;
+
+        foreach (char c in word){
+
+            // Add new node if does not exist
+            if (!currentNode.children.ContainsKey(c)){
+                currentNode.children[c] = new TrieNode();
+            }
+
+            // Move onto next node
+            currentNode = currentNode.children[c];
+        }
+
+        // Make node end of word
+        currentNode.isEndOfWord = true;
+    }
+    
+    public bool Search(string word) {
+        TrieNode currentNode = root;
+        
+        foreach (char c in word){
+
+            // If node does not exist no word exists
+            if (!currentNode.children.ContainsKey(c)){
+                return false;
+            }
+
+            // Move onto next node
+            currentNode = currentNode.children[c];
+        }
+
+        return currentNode.isEndOfWord;
+    }
+    
+    public bool StartsWith(string prefix) {
+        TrieNode currentNode = root;
+        
+        foreach (char c in prefix){
+
+            // If node does not exist no word with prefix exists
+            if (!currentNode.children.ContainsKey(c)){
+                return false;
+            }
+
+            // Move onto next node
+            currentNode = currentNode.children[c];
+        }
+        return true;
+    }
+
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.Insert(word);
+ * bool param_2 = obj.Search(word);
+ * bool param_3 = obj.StartsWith(prefix);
+ */
