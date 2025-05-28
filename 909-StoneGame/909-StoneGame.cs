8ccf4359baaf9f48f7d68820d575deb166ca772e
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:15:57 +0100

    Create 909-StoneGame

---
+++ 909-StoneGame/909-StoneGame.cs
@@ -0,0 +1,20 @@
+// Last updated: 28/05/2025, 13:15:55
+public class Solution {
+    public bool StoneGame(int[] piles) {
+        int n = piles.Length;
+        int[,] dp = new int[n, n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i, i] = piles[i];    
+        }
+
+        for (int d = 1; d < n; d++) { // d is the difference between i and j
+            for (int i = 0; i < n - d; i++) {
+                int j = i + d;
+                dp[i, j] = Math.Max(piles[i] - dp[i + 1, j], piles[j] - dp[i, j - 1]);
+            }
+        }
+
+        return dp[0, n - 1] > 0;
+    }
+}
