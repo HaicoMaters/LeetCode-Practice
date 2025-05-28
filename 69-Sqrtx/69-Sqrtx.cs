9a2f037060c8ba94820ebcf9123f2330252cab92
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:18:34 +0100

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.cs
@@ -0,0 +1,28 @@
+// Last updated: 28/05/2025, 13:18:33
+public class Solution {
+    public int MySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int start = 1; // Start of search area
+        int end = x; // Largest possible square
+
+        while (start <= end)
+        {
+            int midpoint = start + (end - start) / 2;
+            long midSquared = (long)midpoint * midpoint; // Long to avoid overflow
+
+            if (midSquared > x)
+            {
+                end = midpoint - 1;
+            }
+            else if (midSquared == x)
+            {
+                return midpoint;
+            }
+            else {
+                start = midpoint + 1;
+            }
+        }
+        return end;
+    }
+}
