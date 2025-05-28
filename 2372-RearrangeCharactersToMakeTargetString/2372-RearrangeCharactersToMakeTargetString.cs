abd6ce028c7f6cfee8e1cc59a917589acd266a37
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:31 +0100

    Create 2372-RearrangeCharactersToMakeTargetString

---
+++ 2372-RearrangeCharactersToMakeTargetString/2372-RearrangeCharactersToMakeTargetString.cs
@@ -0,0 +1,29 @@
+// Last updated: 28/05/2025, 13:14:30
+public class Solution {
+    public int RearrangeCharacters(string s, string target) {
+        int[] countS = new int[26];
+        foreach (char c in s)
+        {
+            countS[c - 'a']++;
+        }
+
+        int[] countT = new int[26];
+        foreach (char c in target)
+        {
+            countT[c - 'a']++;
+        }
+
+        int minPossible = int.MaxValue;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (countT[i] == 0) continue;
+            if (countS[i] == 0) return 0;
+
+            minPossible = Math.Min(minPossible, countS[i] / countT[i]);
+        }
+        
+
+        return minPossible;
+    }
+}
