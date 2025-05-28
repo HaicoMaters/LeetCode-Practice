5b113092013f39d691afdd37b026230899f582ba
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:18:07 +0100

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cs
@@ -0,0 +1,18 @@
+// Last updated: 28/05/2025, 13:18:06
+public class Solution {
+    public int SingleNumber(int[] nums) {
+
+        int ones = 0; // Tracks the bits that have appeared once
+        int twos = 0; // Tracks the bits that have appeared twice
+    
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            int num = nums[i];
+        // & ~twos means not in twos so the if the bit exists in twos it is not updated when the third of the same appears
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        
+        return ones;
+    }
+}
