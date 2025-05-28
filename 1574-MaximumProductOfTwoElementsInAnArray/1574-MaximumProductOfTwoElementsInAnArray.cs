a6c37733c0bfe2c1f517f530c037f0dbd8893fe5
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:56 +0100

    Create 1574-MaximumProductOfTwoElementsInAnArray

---
+++ 1574-MaximumProductOfTwoElementsInAnArray/1574-MaximumProductOfTwoElementsInAnArray.cs
@@ -0,0 +1,26 @@
+// Last updated: 28/05/2025, 13:14:55
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        int largest = 0;
+        int secondLargest = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            if(num > secondLargest)
+            {
+                if(num > largest)
+                {  
+                    secondLargest = largest;
+                    largest = num;
+                }
+                else
+                {
+                    secondLargest = num;
+                }
+            }
+        }
+
+        return (largest - 1) * (secondLargest -1);
+    }
+}
