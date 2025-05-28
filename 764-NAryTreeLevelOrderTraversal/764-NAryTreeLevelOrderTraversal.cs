771d8e8858dc37bd2cebe1fe69e054b356450f53
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:16:09 +0100

    Create 764-NAryTreeLevelOrderTraversal

---
+++ 764-NAryTreeLevelOrderTraversal/764-NAryTreeLevelOrderTraversal.cs
@@ -0,0 +1,46 @@
+// Last updated: 28/05/2025, 13:16:08
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    public List<IList<int>> order;
+
+    public IList<IList<int>> LevelOrder(Node root) {
+        order = new List<IList<int>>();
+        Traverse(root, 1);
+        return order;
+    }
+
+    public void Traverse(Node current, int depth)
+    {
+        if (current == null) return;
+
+        if (order.Count < depth)
+        {
+            order.Add(new List<int>());
+        }
+
+        order[depth - 1].Add(current.val);
+
+        foreach (Node child in current.children)
+        {
+            Traverse(child, depth + 1);
+        }
+    }
+}
