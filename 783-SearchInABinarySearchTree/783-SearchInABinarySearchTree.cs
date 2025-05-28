842d4996547c290854e115d0605a5069d5534e15
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:16:03 +0100

    Create 783-SearchInABinarySearchTree

---
+++ 783-SearchInABinarySearchTree/783-SearchInABinarySearchTree.cs
@@ -0,0 +1,23 @@
+// Last updated: 28/05/2025, 13:16:02
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        
+        if (root.val > val) return SearchBST(root.left, val);
+        else return SearchBST(root.right, val);
+    }
+}
