ab166e7e414c2fa18f5594f553902522ac8d309a
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:47 +0100

    Create 1839-DecodeXoredArray

---
+++ 1839-DecodeXoredArray/1839-DecodeXoredArray.cs
@@ -0,0 +1,14 @@
+// Last updated: 28/05/2025, 13:14:46
+public class Solution {
+    public int[] Decode(int[] encoded, int first) {
+        int n = encoded.Length + 1;
+        int[] decoded = new int[n];
+        decoded[0] = first;
+
+        for (int i = 1; i < n; i++) {
+            decoded[i] = encoded[i - 1] ^ decoded[i - 1];
+        }
+
+        return decoded;
+    }
+}
