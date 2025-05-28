962c9598af535e3909043bed3b8f1fd3d499169e
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:15:38 +0100

    Create 1287-DistanceBetweenBusStops

---
+++ 1287-DistanceBetweenBusStops/1287-DistanceBetweenBusStops.cs
@@ -0,0 +1,26 @@
+// Last updated: 28/05/2025, 13:15:37
+public class Solution {
+    public int DistanceBetweenBusStops(int[] distance, int start, int destination) {
+        
+        // Only calculate distance going forwards (swapped distance is same as counterclockwise)
+        if (start > destination) {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+
+        int totalDistance = 0;
+        int clockwiseDistance = 0;
+
+        for (int i = 0; i < distance.Length; i++) {
+            if (i >= start && i < destination) {
+                clockwiseDistance += distance[i];
+            }
+            totalDistance += distance[i];
+        }
+
+        int counterClockwiseDistance = totalDistance - clockwiseDistance;
+
+        return Math.Min(clockwiseDistance, counterClockwiseDistance);
+    }
+}
