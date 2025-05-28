e4ebe3df6d315757439d9730b8eef2ce014b611d
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:45 +0100

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.cs
@@ -0,0 +1,16 @@
+// Last updated: 28/05/2025, 13:14:44
+public class Solution {
+    public int FindCenter(int[][] edges) {
+        bool[] hasEdge = new bool[edges.Length + 2]; // Mark each index i is node i
+
+        for (int i = 0; i < edges.Length; i++)
+        {
+            if (hasEdge[edges[i][0]]) return edges[i][0];
+            else hasEdge[edges[i][0]] = true;
+            if (hasEdge[edges[i][1]]) return edges[i][1];
+            else hasEdge[edges[i][1]] = true;
+        }
+
+        return -1;
+    }
+}
