146e33a17f2e4f4621031bb4a262e88b0cf67fac
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:16:22 +0100

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.cs
@@ -0,0 +1,13 @@
+// Last updated: 28/05/2025, 13:16:21
+public class Solution {
+    public string ReverseWords(string s) {
+        string[] words = s.Split(" ");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = new string(words[i].Reverse().ToArray());
+        }
+
+        return string.Join(" ", words);
+    }
+}
