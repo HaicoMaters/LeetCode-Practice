359f677f52687908cf5858f0eaaf7b167728da38
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:17:41 +0100

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.cs
@@ -0,0 +1,15 @@
+// Last updated: 28/05/2025, 13:17:40
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void DeleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
