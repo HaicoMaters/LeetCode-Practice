67c864d74fa0bf16fadf55cb0d78d44166947441
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:17:32 +0100

    Create 299-BullsAndCows

---
+++ 299-BullsAndCows/299-BullsAndCows.cs
@@ -0,0 +1,32 @@
+// Last updated: 28/05/2025, 13:17:31
+public class Solution {
+    public string GetHint(string secret, string guess) {
+        Dictionary<char, int> secretCount = new Dictionary<char, int>();
+
+        int x = 0;
+        int y = 0;
+
+        foreach (char c in secret){
+            if(secretCount.ContainsKey(c)){
+                secretCount[c]++;
+            }
+            else{
+                secretCount[c] = 1;
+            }
+        }
+
+        for (int i = 0; i < guess.Length; i++){
+            char c = guess[i];
+            if (secretCount.ContainsKey(c) && secretCount[c] > 0){
+                y++;
+                secretCount[c]--;
+            }
+            if (c == secret[i]){
+                y--;
+                x++;
+            }
+        }
+
+        return string.Concat(x,"A", y,"B");
+    }
+}
