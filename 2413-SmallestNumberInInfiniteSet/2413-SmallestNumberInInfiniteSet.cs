ba15ef1263b7e2e754e3301d580a40e79ef41298
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:29 +0100

    Create 2413-SmallestNumberInInfiniteSet

---
+++ 2413-SmallestNumberInInfiniteSet/2413-SmallestNumberInInfiniteSet.cs
@@ -0,0 +1,34 @@
+// Last updated: 28/05/2025, 13:14:28
+public class SmallestInfiniteSet {
+    
+    int nextToAdd;
+    SortedSet<int> set;
+
+    public SmallestInfiniteSet() {
+        set = new SortedSet<int>(); 
+        nextToAdd = 1;
+    }
+    
+    public int PopSmallest() {
+        if (set.Count > 0) {
+            int smallest = set.Min;
+            set.Remove(smallest);
+            return smallest;
+        }
+        return nextToAdd++;
+    }
+    
+    public void AddBack(int num) {
+        if (num < nextToAdd) { // Only add back if it was previously popped
+            set.Add(num);
+        }
+    }
+}
+
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.PopSmallest();
+ * obj.AddBack(num);
+ */
