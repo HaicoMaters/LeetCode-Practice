a6c2c8dd1debd3519471bed4622e2ca1f08642da
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:18:16 +0100

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cs
@@ -0,0 +1,23 @@
+// Last updated: 28/05/2025, 13:18:14
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        List<IList<int>> triangle = new List<IList<int>>();
+
+        triangle.Add(new List<int>{1}); // First row;
+
+        for (int i = 1; i < numRows; i++)
+        {
+            List<int> row = new List<int>{1}; // Each row starts with 1
+
+            for (int j = 0; j < triangle[i-1].Count - 1; j++) // Combine all the ones in the middle using row above
+            {
+                row.Add(triangle[i-1][j] + triangle[i-1][j + 1]);
+            }
+
+            row.Add(1); // Ends with a 1
+            triangle.Add(row);
+        }
+
+        return triangle;
+    }
+}
