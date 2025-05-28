ee44025ac258299d072d7073d1991a39914f0290
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:05 +0100

    Create 3321-TypeOfTriangle

---
+++ 3321-TypeOfTriangle/3321-TypeOfTriangle.cs
@@ -0,0 +1,9 @@
+// Last updated: 28/05/2025, 13:14:04
+public class Solution {
+    public string TriangleType(int[] nums) {
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) return "none";
+        if (nums[0] == nums[1] && nums[0] == nums[2]) return "equilateral";
+        if (nums[0] == nums[2] || nums[0] == nums[1] || nums[1] == nums[2]) return "isosceles";
+        return "scalene";
+    }
+}
