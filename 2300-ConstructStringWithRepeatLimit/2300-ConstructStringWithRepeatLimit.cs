40d2034ef0f04a0867a6d655ec7e06f9b862504d
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:14:33 +0100

    Create 2300-ConstructStringWithRepeatLimit

---
+++ 2300-ConstructStringWithRepeatLimit/2300-ConstructStringWithRepeatLimit.cs
@@ -0,0 +1,44 @@
+// Last updated: 28/05/2025, 13:14:32
+public class Solution {
+    public string RepeatLimitedString(string s, int repeatLimit) {
+        int[] freq = new int[26];
+
+        for(int i=0; i<s.Length; i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=25; i>=0; i--)
+        {
+            int pos = i;
+            int limit = Math.Min(freq[pos], repeatLimit);
+
+            for(int j=0; j<limit; j++)
+            {
+                sb.Append((char)((pos) + 'a'));
+                freq[pos]--;
+            }
+
+            if(freq[pos] >= 1)
+            {                
+                pos--;
+
+                while (pos >= 0 && freq[pos] == 0) 
+                {
+                    pos--;
+                }
+
+                if (pos < 0) break; 
+                
+                sb.Append((char)((pos) + 'a'));
+                freq[pos]--;
+                i++;
+            }
+        }
+
+        return sb.ToString();
+        
+    }
+}
