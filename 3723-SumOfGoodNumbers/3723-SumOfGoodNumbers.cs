e485ec694384c6cfceaae859ab7f8dc9a05445ef
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:13:57 +0100

    Create 3723-SumOfGoodNumbers

---
+++ 3723-SumOfGoodNumbers/3723-SumOfGoodNumbers.cs
@@ -0,0 +1,24 @@
+// Last updated: 28/05/2025, 13:13:56
+public class Solution {
+    public int SumOfGoodNumbers(int[] nums, int k) {
+        int sum = 0;
+        int n = nums.Length;
+
+        for (int i = 0; i < n; i++) 
+        {
+            int val = nums[i];
+            bool firstCheck = true;
+            bool secondCheck = true;
+
+            if (i >= k) firstCheck = val > nums[i - k];
+            if (i + k < n) secondCheck = val > nums[i + k]; 
+
+            if (firstCheck && secondCheck) 
+            {
+                sum += val;
+            }
+        }
+
+        return sum;
+    }
+}
