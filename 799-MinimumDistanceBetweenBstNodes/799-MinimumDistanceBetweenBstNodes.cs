76cdf0ba295d85d8d0e63de9d8db63a8177f3571
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-05-28 13:16:01 +0100

    Create 799-MinimumDistanceBetweenBstNodes

---
+++ 799-MinimumDistanceBetweenBstNodes/799-MinimumDistanceBetweenBstNodes.cs
@@ -0,0 +1,33 @@
+// Last updated: 28/05/2025, 13:16:00
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {    
+    int minDiff = int.MaxValue;
+    TreeNode prev = null;
+   
+    public int MinDiffInBST(TreeNode root) {
+        DFS(root);
+        return minDiff;
+    }
+
+    public void DFS(TreeNode current)
+    {
+        if (current == null) return;
+
+        DFS(current.left);
+        if (prev != null) minDiff = (Math.Min(minDiff, current.val - prev.val));
+        prev = current;
+        DFS(current.right);
+    }
+}
