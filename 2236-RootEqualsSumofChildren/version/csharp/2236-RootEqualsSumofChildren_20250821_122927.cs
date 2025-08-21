8388037a57d3c77ccd770fedc167b888d4306a58
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-08-21 12:29:30 +0100

    Create 2236-RootEqualsSumofChildren

---
+++ 2236-RootEqualsSumofChildren/version/csharp/2236-RootEqualsSumofChildren_20250821_122927.cs
@@ -0,0 +1,20 @@
+// Last updated: 21/08/2025, 12:29:27
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool CheckTree(TreeNode root) {
+        if (root.left == null || root.right == null) return false;
+        return root.val == (root.left.val + root.right.val);
+    }
+}
