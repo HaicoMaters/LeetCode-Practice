5e09766b213a13f468fdc8bd53ec16e6d5d12718
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-09-11 11:23:42 +0100

    Create 2785-SortVowelsinaString

---
+++ 2785-SortVowelsinaString/version/csharp/2785-SortVowelsinaString_20250911_112339.cs
@@ -0,0 +1,30 @@
+// Last updated: 11/09/2025, 11:23:39
+public class Solution {
+    public string SortVowels(string s) {
+        HashSet<char> vowelSet = new HashSet<char>("AEIOUaeiou");
+        List<char> vowels = new List<char>();
+
+        foreach (char c in s)
+        {
+            if (vowelSet.Contains(c))
+            {
+                vowels.Add(c);
+            }
+        }
+
+        vowels.Sort();
+
+        char[] str = s.ToCharArray();
+        int idx = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (vowelSet.Contains(str[i]))
+            {
+                str[i] = vowels[idx++];
+            }
+        }
+
+        return new string(str);
+    }
+}
