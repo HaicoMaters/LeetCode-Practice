d5107705322f53d42bced46c59844c2983e0ad5c
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-10-07 11:35:28 +0100

    Create 2769-FindtheMaximumAchievableNumber

---
+++ 2769-FindtheMaximumAchievableNumber/version/csharp/2769-FindtheMaximumAchievableNumber_20251007_113528.cs
@@ -0,0 +1,6 @@
+// Last updated: 07/10/2025, 11:35:28
+public class Solution {
+    public int TheMaximumAchievableX(int num, int t) {
+        return num + (t * 2); // can get 2 closer each time
+    }
+}
