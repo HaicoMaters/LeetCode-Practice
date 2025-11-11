551de60414cd096957e164c3f2a76a45cd2fbe82
HaicoMaters <91530082+HaicoMaters@users.noreply.github.com>
2025-11-11 12:04:20 +0000

    Create 474-OnesandZeroes

---
+++ 474-OnesandZeroes/version/csharp/474-OnesandZeroes_20251111_120416.cs
@@ -0,0 +1,24 @@
+// Last updated: 11/11/2025, 12:04:16
+public class Solution {
+    public int FindMaxForm(string[] strs, int m, int n) {
+        int[,] dp = new int[m + 1, n + 1];
+
+        foreach (string str in strs){
+            int zeros = 0;
+            int ones = 0;
+
+            foreach (char c in str){
+                if (c == '0') zeros++;
+                else if (c == '1') ones++;
+            }
+
+            for (int i = m; i >= zeros; i--){
+                for (int j = n; j >= ones; j--)
+                {
+                    dp[i, j] = Math.Max(dp[i,j], dp[i-zeros, j-ones] + 1);
+                }
+            }
+        }
+        return dp[m,n];
+    }
+}
